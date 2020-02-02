e711bc2f659cdaff0bca33741d79815fc922f003
modeco80 <lily.modeco80@protonmail.ch>
2020-02-02 16:12:48 -0500

    Document PerIntervalRunner and make it slightly more robust

--- src/PerIntervalRunner.hpp
+++ src/PerIntervalRunner.hpp
@@ -21,7 +21,10 @@ namespace mdrpc {
         }
 
         /**
-         * Start a function without any arguments
+         * Start calling a function on an interval without arguments.
+         * 
+         * \param[in] interval Interval to call function,
+         * \param[in] fun Function to call on an interval
          */
         template<class F>
         void Start(std::uint16_t interval, F fun) {
@@ -29,20 +32,37 @@ namespace mdrpc {
         }
 
         /**
-         * Start a function with arguments
+         * Start calling a function on an interval with arguments.
+         * 
+         * \param[in] interval Interval to call function,
+         * \param[in] fun Function to call on an interval
+         * \param[in] args All arguments to pass to the function
          */
         template<class F, class ...Args>
         void Start(std::uint16_t interval, F fun, Args... args) {
             runner_thread.reset(new std::thread(std::bind(&PerIntervalRunner::Runner<F, Args...>, this, interval, fun, std::forward<Args...>(args...))));
         }
 
+        /**
+         * Stop the current runner.
+         */ 
         void Stop() {
+            if(!started)
+                return;
+            
             stop_mutex.lock();
             stop = true;
             stop_mutex.unlock();
         }
+
     private:
 
+        /**
+         * Generic function for the the thread to run.
+         * 
+         * \param[in] interval Interval (in milliseconds) passed by Start<F>()
+         * \param[in] fun Function to call
+         */
         template<class F>
         void Runner(std::uint16_t interval, F fun) {
             started = true;
@@ -59,6 +79,12 @@ namespace mdrpc {
             started = false;
         }
 
+        /**
+         * Generic function for the the thread to run.
+         * 
+         * \param[in] interval Interval (in milliseconds) passed by Start<F, Args...>()
+         * \param[in] fun Function to call
+         */
         template<class F, class ...Args>
         void Runner(std::uint16_t interval, F fun, Args... args) {
             started = true;
@@ -75,10 +101,22 @@ namespace mdrpc {
             started = false;
         }
 
+        /**
+         * Runner thread.
+         */ 
         std::shared_ptr<std::thread> runner_thread;
+
         bool started = false;
 
+        /**
+         * Lock for the `stop` variable
+         */
         std::mutex stop_mutex;
+
+        /**
+         * Indicates that the runner function should stop and kill the thread,
+         * stopping execution of the user's specified function.
+         */
         bool stop = false;
     };
 
